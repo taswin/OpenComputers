0cb4fdc5310e04a8f83d57b0b9adaab5e2561d57
taswin <taswin@users.noreply.github.com>
2016-03-29 13:01:13 +1300

    Update autorun.lua

--- Floppy/autorun.lua
+++ Floppy/autorun.lua
@@ -1,27 +1,28 @@
 local component = require("component")
+
 if component.isAvailable("br_reactor") then
   local reactor = component.br_reactor
 end
 
 if component.isAvailable("modem") then
-local modem = component.modem
+  local modem = component.modem
 end
 
 if component.isAvailable("tunnel") then
-local tunnel = component.tunnel
+  local tunnel = component.tunnel
+end
+
+if component.isAvailable("term") then
+  local term = component.term
 end
 
 local args = {...}
 local port = tostring(args[1])
 
-if recator then
-  if modem then
+if modem or tunnel then
+  if reactor then
   os.execute("BRC.lua " .. port)
-  else if tunnel then
-  os.execute("BRC.lua " .. port)
+  elseif term then
+  os.execute("Display.lua " .. port)
   end
-else if modem then
-  os.execute("Display.lua " .. port)
-else if tunnel then
-  os.execute("Display.lua " .. port)
 end

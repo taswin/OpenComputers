566dbccf709282a14c120b17b299b70ce25d49c1
taswin <taswin@users.noreply.github.com>
2016-03-29 13:07:39 +1300

    Update autorun.lua

--- Floppy/autorun.lua
+++ Floppy/autorun.lua
@@ -19,10 +19,18 @@ end
 local args = {...}
 local port = tostring(args[1])
 
-if modem or tunnel then
-  if reactor then
-  os.execute("BRC.lua " .. port)
-  elseif term then
-  os.execute("Display.lua " .. port)
+if port then
+  if modem or tunnel then
+    if reactor then
+      os.execute("BRC.lua " .. port)
+    elseif term then
+      os.execute("Display.lua " .. port)
+    else
+      print("No reactor or terminal found!")
+    end
+  else
+    print("No network or link cards found!")
   end
+else
+  print("No port given!")
 end

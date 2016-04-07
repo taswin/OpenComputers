08be08854aec8a1579b9bd127ba97ce54c262dec
taswin <taswin@users.noreply.github.com>
2016-04-07 14:34:07 +1200

    Create stargate.lua

---
+++ Floppy/stargate.lua
@@ -0,0 +1,60 @@
+local component = require("component")
+local sg = component.stargate
+
+function dial(address)
+  return sg.connect(address)
+end
+
+function terminate()
+  sg.disconnect()
+end
+
+function getLocalState()
+  if(sg.isActive) then
+    if(sg.isDialing) then
+      return "dialing"
+   else
+     return "connected"
+    end
+  else
+    return "disconnected"
+  end
+end
+
+function getLocalFuelFound()
+  return sg.hasFuel()
+end
+
+function getLocalAddress()
+  return sg.getAddress()
+end
+
+function gerRemoteState(address)
+  if (sg.isBusy(address))
+    return "busy"
+  else
+    return "available"
+  end
+end
+
+function unknownEvent()
+end
+
+local eventHandlers = setmetatable({}, { __index = function() return unknownEvent end })
+
+function eventHandlers.sgIncommung(glyph)
+  --
+end
+
+function eventHandlers.sgOutgoing(address)
+  --
+end
+
+function handleEvent(eventID, ...)
+  if (eventID) then
+    eventHandlers[eventID](...)
+  end
+end
+
+event.listen("sgIncomming", handleEvent)
+event.listen("sgOutgoing", handleEvent)
